15a9f833d079a08f9386ba662ea46a5e2a39cbee
‘mr’ <1157836448mr@163.com>
2022-11-01 13:29:03 +0800

    all of parameters and model

---
+++ RNNmodel.py
@@ -0,0 +1,60 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+
+
+# 超参数设置
+class Config(object):
+
+    """配置参数"""
+    def __init__(self, dataset, embedding):
+        self.model_name = 'TextRNN'
+        self.train_path = dataset + '/data/train.txt'                                # 训练集
+        self.dev_path = dataset + '/data/dev.txt'                                    # 验证集
+        self.test_path = dataset + '/data/test.txt'                                  # 测试集
+        self.class_list = [x.strip() for x in open(
+            dataset + '/data/class.txt').readlines()]                                # 类别列表
+        self.vocab_path = dataset + '/data/vocab.pkl'                                # 词表
+        self.save_path = dataset + '/saved_dict/' + self.model_name + '.ckpt'        # 模型训练结果
+        self.embedding_pretrained = torch.tensor(
+            np.load(dataset + '/data/' + embedding)["embeddings"].astype('float32')) # 预训练词向量
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')   # 设备
+
+        self.dropout = 0.5                                              # 随机失活
+        self.require_improvement = 1000                                 # 若超过1000batch效果还没提升，则提前结束训练
+        self.num_classes = len(self.class_list)                         # 类别数
+        self.n_vocab = 0                                                # 词表大小，在运行时赋值
+        self.num_epochs = 10                                            # epoch数
+        self.batch_size = 128                                           # mini-batch大小
+        self.pad_size = 32                                              # 每句话处理成的长度(短填长切)
+        self.learning_rate = 1e-3                                       # 学习率
+        self.embed = self.embedding_pretrained.size(1)                  # 词向量维度, 若使用了预训练词向量，则维度统一
+        self.hidden_size = 128                                          # lstm隐藏层
+        self.num_layers = 2                                             # lstm层数
+
+
+'''Recurrent Neural Network for Text Classification with Multi-Task Learning'''
+
+
+class RNNModel(nn.Module):
+    def __init__(self, config):
+        super(RNNModel, self).__init__()
+        # 使用预训练的词向量模型，freeze=False 表示允许参数在训练中更新
+        self.embedding = nn.Embedding.from_pretrained(config.embedding_pretrained, freeze=False)
+        # bidirectional=True表示使用的是双向LSTM
+        self.lstm = nn.LSTM(config.embed, config.hidden_size, config.num_layers,
+                            bidirectional=True, batch_first=True, dropout=config.dropout)
+        # 因为是双向LSTM，所以层数为config.hidden_size * 2
+        self.fc = nn.Linear(config.hidden_size * 2, config.num_classes)
+
+    def forward(self, x):
+        out = self.embedding(x)  # [batch_size, seq_len, embeding]=[128, 32, 300]
+        # lstm 的input为[batchsize, max_length, embedding_size]，输出表示为 output,(h_n,c_n),
+        # 保存了每个时间步的输出，如果想要获取最后一个时间步的输出，则可以这么获取：output_last = output[:,-1,:]
+        out, _ = self.lstm(out)
+        out = self.fc(out[:, -1, :])  # 句子最后时刻的 hidden state
+        return out
+
+
